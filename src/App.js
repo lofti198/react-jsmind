ccf9e807b2bf62a7da011e17311d43e5adc64efd
Meruzh <meruzh.kiloyan@mail.ru>
2022-08-22 17:33:16 +0400

    initial version of working js-mind with react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,64 @@
+import React from 'react';
+import JSMindMM from "./components/JSMindMM";
 import logo from "./logo.svg";
 import "./App.css";
-import JSMindMM from "./components/JSMindMM";
+import 'jsmind/style/jsmind.css';
+
+const mind = {
+    "version": 1,
+    "meta": {
+        "name": "demo",
+        "author": "hizzgdev@163.com",
+        "version": "0.2",
+    },
+    "format": "node_array",
+    "data": [
+        { "id": "root", "isroot": true, "topic": '<div id="20170919"></div>' },
+        { "id": "sub1", "parentid": "root", "topic": "sub1111111", "background-color": "#0000ff" },
+        { "id": "sub11", "parentid": "sub1", "topic": "sub11" },
+        { "id": "sub12", "parentid": "sub1", "topic": "sub12" },
+        { "id": "sub13", "parentid": "sub1", "topic": "sub13" },
+        { "id": "sub2", "parentid": "root", "topic": "sub2" },
+        { "id": "sub21", "parentid": "sub2", "topic": "sub21" },
+        { "id": "sub22", "parentid": "sub2", "topic": "sub22", "foreground-color": "#33ff33" },
+        { "id": "sub3", "parentid": "root", "topic": "sub3" },
+    ]
+};
+const options = {
+    container: 'jsmind_container',
+    theme: 'orange',
+    editable: true,
+    support_html: true,
+    support_react: true,
+};
+
 function App() {
-  return (
-    <>
-      <JSMindMM />
-    </>
-  );
+    const handleContextMenu = (e, jmnode) => {
+
+    }
+    const handleMouseDown = (e, jmnode) => {
+
+    }
+    const handleMouseUp = (e, jmnode) => {
+
+    }
+    const handleClick = (e, jmnode) => {
+
+    }
+
+    return (
+        <>
+            <JSMindMM
+                mind={mind}
+                options={options}
+                styles={{ width: '1000px', height: '600px' }}
+                onClick={handleClick}
+                onContextMenu={handleContextMenu}
+                onMouseDown={handleMouseDown}
+                onMouseUp={handleMouseUp}
+            />
+        </>
+    );
 }
 
 export default App;
--- src/components/JSMindMM.js
+++ src/components/JSMindMM.js
@@ -1,59 +1,44 @@
 import React, { useEffect } from "react";
-import { jm as jsMind } from "jsmind";
+import jm from "jsmind/js/jsmind";
 
-const JSMindMM = () => {
-  useEffect(() => load_jsmind(), []);
-  function load_jsmind() {
-    var mind = {
-      meta: {
-        name: "demo",
-        author: "hizzgdev@163.com",
-        version: "0.2",
-      },
-      format: "node_array",
-      data: [
-        { id: "root", isroot: true, topic: "jsMind" },
+const JSMindMM = ({
+    mind,
+    styles,
+    options,
+    onClick,
+    onMouseUp,
+    onMouseDown,
+    onContextMenu
+}) => {
 
-        {
-          id: "sub1",
-          parentid: "root",
-          topic: "sub1",
-          "background-color": "#0000ff",
-        },
-        { id: "sub11", parentid: "sub1", topic: "sub11" },
-        { id: "sub12", parentid: "sub1", topic: "sub12" },
-        { id: "sub13", parentid: "sub1", topic: "sub13" },
+    useEffect(() => {
+        const jmm = new jm(options);
+        jmm.show(mind);
+    }, []);
 
-        { id: "sub2", parentid: "root", topic: "sub2" },
-        { id: "sub21", parentid: "sub2", topic: "sub21" },
-        {
-          id: "sub22",
-          parentid: "sub2",
-          topic: "sub22",
-          "foreground-color": "#33ff33",
-        },
-
-        { id: "sub3", parentid: "root", topic: "sub3" },
-      ],
-    };
-    var options = {
-      container: "jsmind_container",
-      editable: true,
-      theme: "primary",
-    };
-    var jm = jsMind.show(options, mind);
-    // jm.set_readonly(true);
-    // var mind_data = jm.get_data();
-    // alert(mind_data);
-    jm.add_node("sub2", "sub23", "new node", { "background-color": "red" });
-    jm.set_node_color("sub21", "green", "#ccc");
-  }
-
-  return (
-    <>
-      <div id="jsmind_container"></div>
-    </>
-  );
+    return (
+        <div
+            id="jsmind_container"
+            // onClick={(e) => {
+            //     const realSelectNode = new jm(options).getNode(e);
+            //     onClick(e, realSelectNode);
+            // }}
+            // onMouseDown={(e) => {
+            //     const realSelectNode = new jm(options).getNode(e);
+            //     onMouseDown(e, realSelectNode);
+            // }}
+            // onMouseUp={(e) => {
+            //     const realSelectNode = new jm(options).getNode(e);
+            //     onMouseUp(e, realSelectNode);
+            // }}
+            // onContextMenu={(e) => {
+            //     const realSelectNode = new jm(options).getNode(e);
+            //     onContextMenu(e, realSelectNode);
+            // }}
+            style={styles}
+        >
+        </div>
+    );
 };
 
 export default JSMindMM;
